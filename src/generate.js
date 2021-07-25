ff10847b5ff92d28d5d65d0a2e9c3214c631675d
Eduardo Sanchez <eduardo.daniel.s.m@gmail.com>
2021-07-25 14:27:02 -0400

    feat(generate.js): Update logic to reduce complexity

--- src/generate.js
+++ src/generate.js
@@ -7,24 +7,37 @@ const path = "./dist";
 const name = "anti-cookies";
 const file = `./src/template/${name}.js`;
 const encoding = "utf8";
+const jsFlag = "[JS]";
 
+/* File process and replacement */
+const isElement = item => !!~item.indexOf(jsFlag);
+const removeFlag = item => item.substr(jsFlag.length);
+const appendInclude = item => `// @include      ${item}`;
+const wrap = item => `\"${item}\"`;
+const shouldWrap = i => isElement(i) ? removeFlag(i) : wrap(i);
 const getReplacement = (info, key) => info?.[key] ?? defaultInfo[key];
-
-const content = await getFileString(file, encoding);
-
+const getFunc = obj => key => getReplacement(obj, key);
+const processIncludesFunc = func => key => func(key).map(appendInclude).join("\n");
+const getElementsFunc = func => key => func(key).map(shouldWrap).join(",\n    ");
 const replaceText = (string, info) => {
-  const get = key => getReplacement(info, key);
-  const wrap = key => get(key).map(item => `\"${item}\"`);
+  const get = getFunc(info);
+  const getElements = getElementsFunc(get);
+  const processIncludes = processIncludesFunc(get);
   return string
     .replaceAll("{{NAME}}", get("NAME"))
     .replaceAll("{{WEB_PAGE}}", get("WEB_PAGE"))
     .replaceAll("{{MATCH}}", get("MATCH"))
     .replaceAll(`"{{MAX}}"`, get("MAX"))
+    .replaceAll(`"{{CICLES}}"`, get("CICLES"))
+    .replaceAll(`"{{INITIAL_DELAY}}"`, get("INITIAL_DELAY"))
     .replaceAll(`"{{RETRY_TIME}}"`, get("RETRY_TIME"))
-    .replaceAll("//{{INCLUDES}}//", get("INCLUDES").join("\n"))
-    .replaceAll("//{{PARENTS}}//", wrap("PARENTS").join(",\n    "))
-    .replaceAll("//{{TARGETS}}//", wrap("TARGETS").join(",\n    "));
+    .replaceAll(`"{{LOOP}}"`, get("LOOP"))
+    .replaceAll("//{{INCLUDES}}//", processIncludes("INCLUDES"))
+    .replaceAll("//{{PARENTS}}//", getElements("PARENTS"))
+    .replaceAll("//{{TARGETS}}//", getElements("TARGETS"));
 };
+
+const content = await getFileString(file, encoding);
 
 const processFile = info => {
   return [
