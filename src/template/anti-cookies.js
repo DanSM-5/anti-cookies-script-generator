d038b78a74012d21fe097ab3909fb87aa751d551
Eduardo Sanchez <eduardo.daniel.s.m@gmail.com>
2021-07-25 14:18:15 -0400

    feat(anti-cookies.js): Support for extra features

--- src/template/anti-cookies.js
+++ src/template/anti-cookies.js
@@ -11,27 +11,35 @@
 (() => {
   const name = "{{NAME}}";
   const label = `Anti-Cookies ${name}`;
-  console.log(`Running ${label}`);
-
+  
   const max = "{{MAX}}"; // number of retries
   const retryTime = "{{RETRY_TIME}}"; // in seconds
+  const cicles = "{{CICLES}}";
+  const initialDelay = "{{INITIAL_DELAY}}";
+  const loop = "{{LOOP}}";
   const targets = [
     // Add here the css selectors of the elements to remove
     //{{TARGETS}}//
   ];
-
   const parentElements = [
     // add here other elements that may need to be unblocked
     // string or element
     //{{PARENTS}}//
   ];
+  
+  const getLogger = logLvlFunc => msg => logLvlFunc(`${label}: ${msg}`);
+  const log = getLogger(console.log);
+  const warn = getLogger(console.warn);
+  log(`Running`);
+
+  const getElement = el => typeof el === "string"
+    ? document.querySelector(el) ?? null
+    : el;
 
   const setOverflowAuto = element => element.style.overflow = "auto";
 
   const unblockElement = el => {
-    const element = typeof el === "string"
-      ? document.querySelector(el) ?? null
-      : el;
+    const element = getElement(el);
 
     if (element) {
       setOverflowAuto(element);
@@ -39,7 +47,7 @@
   };
 
   const removeElement = selector => {
-    const overlay = document.querySelector(selector);
+    const overlay = getElement(selector);
     if (!overlay) return false;
     overlay.parentElement.removeChild(overlay);
     return true;
@@ -53,27 +61,67 @@
     remove(count);
   };
 
-  const onFailGenerator = selector => () => console.log(`Not found ${selector}`);
+  const failMessage = selector => log(`Not found [${selector}]`);
 
-  const processTarget = selector => {
-    console.log(`Trying to remove ${selector}`);
-    const onFail = onFailGenerator(selector);
+  const shouldLoop = () => {
+    if (loop) {
+      warn("Looping is active");
+      return true;
+    }
+    return false;
+  };
+
+  const restartCicle = (retryFunc, selector) => cicle => {
+    if (shouldLoop() || cicle < cicles) {
+      log(`Starting cicle ${cicle + 1} for [${selector}]`);
+      retryFunc(0);
+    }
+  };
+
+  const trackCicles = (retry, selector) => {
+    let ci = 0;
+    return _ => restartCicle(retry, selector)(++ci);
+  };
+
+  const getFunctionsForSelector = selector => {
+    const onFail = () => {
+      failMessage(selector);
+      restartCicleIfNeeded();
+    };
     const remove = count => {
       const success = removeElement(selector);
       if (success) {
         parentElements.forEach(unblockElement);
-        console.log(`Target ${selector} was removed`);
+        log(`Target [${selector}] was removed`);
+        restartCicleIfNeeded();
       } else {
-        setTimeout(() => tryRemove(remove, onFail, count + 1), retryTime * 1000);
+        retry(count + 1);
       }
     };
+    const retry = count => setTimeout(() => 
+      tryRemove(remove, onFail, count), retryTime * 1000);
+    const restartCicleIfNeeded = trackCicles(retry, selector);
+
+    return [ remove, onFail ];
+  };
+
+  const processTarget = selector => {
+    log(`Trying to remove [${selector}]`);
+    const [ remove, onFail ] = getFunctionsForSelector(selector);
     tryRemove(remove, onFail, 0);
   };
 
   const initRemoveProcess = () => {
-    targets.forEach(processTarget);
+    setTimeout(_ => 
+      targets.forEach(processTarget),
+      initialDelay * 1000
+    );
   };
 
+  if (loop) {
+    warn("Loop is active, be careful!");
+  }
+
   initRemoveProcess();
-  console.log(`Ending ${label}`);
+  log(`Ending ${label}`);
 })();
